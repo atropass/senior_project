856681b274300008473bad00827a4bf80cda7b15
Alexandr <Alexandr.Alpatov@externals.adidas.com>
2025-04-22 18:51:12 +0500

    Add retries to tts requests

--- backend/app/utils/text_to_speech.py
+++ backend/app/utils/text_to_speech.py
@@ -23,6 +23,28 @@ def get_pronunciation(word: str):
             "text": word
         }
     )
+    response_dict = response.json()
+    # Check if the request was successful
+    if 'detail' in response_dict:
+        for i in range(10):
+            print(f"Attempt {i + 1}: Retrying pronunciation request...")
+            response = requests.post(
+                url=api_url,
+                headers={
+                    'Authorization': f"Bearer {api_key}",
+                    'Content-Type': 'application/json',
+                },
+                json={
+                    'source_language': 'kaz',
+                    'target_language': 'kaz',
+                    'text': word
+                }
+            )
+            response_dict = response.json()
+            if 'detail' not in response_dict:
+                break
+            if i == 9:
+                raise Exception(f"Error fetching pronunciation after 10 attempts: {response.status_code} - {response.text}")
 
     if response.status_code == 200:
         return response.json()
